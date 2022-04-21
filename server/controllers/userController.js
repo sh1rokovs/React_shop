6454b595e0ec42e8a1bebe845b977c23aaf715cd
sh1rokovs <hilallod@yandex.ru>
2022-04-21 17:38:09 +0300

    types&&brands&&controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+const ApiError = require('../error/ApiError')
+
 class UserController {
     async registration(req, res) {
 
@@ -7,7 +9,13 @@ class UserController {
 
     }
 
-    async check(req, res) {
-        
+    async check(req, res, next) {
+        const {id} = req.query
+        if (!id) {
+            return next(ApiError.badRequest('Не задан id'))
+        }
+        res.json(id)
     }
 }
+
+module.exports = new UserController()
